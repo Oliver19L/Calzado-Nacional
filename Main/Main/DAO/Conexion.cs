97fae8577e570d57bd14318bfbf19651d67f1740
Oliver19L <58959615+Oliver19L@users.noreply.github.com>
2019-12-17 20:25:36 -0600

    Haciendo Conexion

---
+++ Main/Main/DAO/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Main.DAO
+{
+    class Conexion
+    {
+        protected SqlConnection connect = new SqlConnection();
+
+        public Conectar(String user , String Pass)
+        {
+            try
+            {
+                connect = new SqlConnection("Server=OLIVERPC\\SQLSERVER2019;Database=Cifrado;UID=" +user+";PWD="+Pass);
+                connect.Open();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        
+
+        public void Listar(DataGridView GridView1)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader leer;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "MostrarUsuarios";
+            cmd.Connection = connect;
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            GridView1.DataSource = dt;
+        }
+    }
+}
